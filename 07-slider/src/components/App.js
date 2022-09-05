464a76aa4ecd93118fdcd9ce82d0abf1903b6d1f
maodesigns <mao.devv@gmail.com>
2022-09-05 09:03:22 +0100

    Updated!

--- 07-slider/src/components/App.js
+++ 07-slider/src/components/App.js
@@ -1,24 +1,76 @@
 // Import React
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-// Import Components
-import Slider from "./Review";
+import data from "../data";
+
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 // Read a Component
 function App() {
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    const lastIndex = data.length - 1;
+    if (index < 0) setIndex(lastIndex);
+    if (index > lastIndex) setIndex(0);
+  }, [index, data]);
+
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <main>
-      <section className="container">
+      <section className="section">
         <div className="title">
           <h2>
-            <span className="">/</span> reviews
+            <span>/</span>reviews
           </h2>
           <div className="underline"></div>
         </div>
-        <Slider />
+        <div className="section-center">
+          {data.map((people, personIndex) => {
+            const { id, name, title, quote, image } = people;
+            let position = "nextSlide";
+
+            if (personIndex === index) position = "activeSlide";
+            if (
+              personIndex === index - 1 ||
+              (index === 0 && personIndex === data.length - 1)
+            )
+              position = "lastSlide";
+
+            return (
+              <article key={id} className={position}>
+                <img className="person-img" src={image} alt={name} />
+                <h4>{name}</h4>
+                <p>{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon"></FaQuoteRight>
+              </article>
+            );
+          })}
+          <button
+            className="prev"
+            onClick={() => {
+              setIndex(index - 1);
+            }}
+          >
+            <FaChevronLeft />
+          </button>
+          <button
+            className="next"
+            onClick={() => {
+              setIndex(index + 1);
+            }}
+          >
+            <FaChevronRight />
+          </button>
+        </div>
       </section>
     </main>
   );
 }
-
 export default App;
